1174b45bee8def9b2eedd7625a880f33e61299eb
luxita <dabuluxita1@yahoo.com>
2024-10-28 11:04:41 +0100

    ok

--- App.jsx
+++ App.jsx
@@ -2,14 +2,48 @@ export default () => {
   const [gameOver, setGameOver] = React.useState(false);
   const [board, setBoard] = React.useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = React.useState("X");
+  const [winningPlayer, setWinningPlayer] = React.useState("It's a draw");
+  const winningMooves = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ];
 
   const handleClick = (cellIndex) => () => {
-    const newBoard = board.map((cell, i) =>
-      i == cellIndex ? currentPlayer : cell
-    );
+    if (board[cellIndex] == null) {
+      let isGameOver = false;
 
-    setBoard(newBoard);
-    setCurrentPlayer(currentPlayer == "X" ? "O" : "X");
+      const newBoard = board.map((cell, i) =>
+        i == cellIndex ? currentPlayer : cell
+      );
+
+      for (const moove of winningMooves) {
+        if (
+          newBoard[moove[0]] == currentPlayer &&
+          newBoard[moove[1]] == currentPlayer &&
+          newBoard[moove[2]] == currentPlayer
+        ) {
+          isGameOver = true;
+          setWinningPlayer(`${currentPlayer} wins !`);
+          setGameOver(true);
+          break;
+        }
+      }
+
+      if (
+        !isGameOver &&
+        newBoard.reduce((a, v) => (v !== null ? a + 1 : a), 0) == 9
+      )
+        setGameOver(true);
+
+      setBoard(newBoard);
+      setCurrentPlayer(currentPlayer == "X" ? "O" : "X");
+    }
   };
 
   return (
@@ -23,10 +57,8 @@ export default () => {
             key={cellIndex}
             class="bg-black cursor-pointer"
           >
-            {cellIndex}
-
-            {cell == "X" && <div class="text-[70px]">X</div>}
-            {cell == "O" && <div class="text-[70px]">O</div>}
+            {cell == "X" && <div class="text-[70px] items-center">X</div>}
+            {cell == "O" && <div class="text-[70px] items-center">O</div>}
           </div>
         ))}
       </div>
@@ -36,7 +68,7 @@ export default () => {
           <h2>GAME OVER</h2>
           <br />
           <h3 className="text-center">
-            It's a draw <br />
+            {winningPlayer} <br />
             <br />
             Refresh to play again
           </h3>
